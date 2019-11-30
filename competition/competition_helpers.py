03d2b17041ea363b0c2357c1e09f97d2f2f8867b
Arvind Thirumurugan <athirumu@buffalo.edu>
2019-11-30 16:55:08 -0500

    Implemented standardiztion

--- competition/competition_helpers.py
+++ competition/competition_helpers.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
+from sklearn import preprocessing
 
 
 def read_csv(file_name, remove_header=None):
@@ -12,7 +13,7 @@ def read_csv(file_name, remove_header=None):
     return np.array(data.iloc[:, 1:].copy())
 
 
-def kfold_stratified_split(X, y, k):
+def kfold_stratified_split(X, y, k, standardize_flag):
 
     skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=42)
 
@@ -21,9 +22,18 @@ def kfold_stratified_split(X, y, k):
     for train, test in skf.split(X, y):
         new_X = X[train, :].copy()
         new_y = y[train, :].copy()
-
+        
         new_X_test = X[test, :].copy()
         new_y_test = y[test, :].copy()
+        if(standardize_flag):
+            new_X,new_X_test = standardize_data(new_X,new_X_test)
         splits.append([(new_X, new_y), (new_X_test, new_y_test)])
 
     return splits
+
+def standardize_data(X_train,X_test):
+    scaler = preprocessing.StandardScaler().fit(X_train)
+    X_train_standardized = scaler.transform(X_train)
+    X_test_standardized = scaler.transform(X_test)
+    return X_train_standardized,X_test_standardized
+    
